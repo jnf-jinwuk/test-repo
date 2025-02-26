a336cfc7b7bb2fc87ed8e7229614d3df3d890d56
useremail in local <jinwuk@jinwuks-MacBook-Pro.local>
2025-02-26 15:53:12 +0900

    new commit message for the baseball player

--- src/main/java/org/cjw/baseball/Main.java
+++ src/main/java/org/cjw/baseball/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("hello baseball");
 		int another = 222;
-		
 		int other = 777;
+    Player player1 = new BaseballPlayer();
 	}
 }
